baa141ac51ce52b30c23493f2039a908144703b8
ilyes arous <ilyesarous9645@gamil.com>
2025-02-10 22:51:49 +0100

    petopia_backend: create the DB petopia_frontend: bootstrap included, navbar and 404 not found are created

---
+++ petopia_backend/src/main/java/tn/esprit/petopia_backend/PetopiaBackendApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.petopia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetopiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetopiaBackendApplication.class, args);
+	}
+
+}
---
+++ petopia_frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
